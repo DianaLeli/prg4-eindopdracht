8258066478399924a8ea8abc4c296e7b79bb0995
DianaLeli <dianaleliveld@gmail.com>
2026-05-28 12:47:48 +0200

    Delete resources and startgame

--- src/js/resources.js
+++ src/js/resources.js
@@ -2,12 +2,8 @@ import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 
 // voeg hier jouw eigen resources toe
 const Resources = {
-    Fish: new ImageSource('images/fish.png'),
-    Shark: new ImageSource('images/shark.png')
+    
 }
-
-
-
 
 const ResourceLoader = new Loader()
 for (let res of Object.values(Resources)) {
